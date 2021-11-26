d05e5e517ad7b836680d3c58667a5372c6d6c0a5
YEISON DAVID BENJUMEA GUTIERREZ <yeisonbenjumea187359@correo.itm.edu.co>
2021-11-25 23:03:19 -0500

    Screens email

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,11 +8,50 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _email = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Login screen')),
-      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _showLogo(),
+            _showEmail(),
+          ],
+        )
+      ),
     );
   }
+
+  Widget _showLogo() {
+    return Image(
+      image: AssetImage('assets/Sucuamorlogo.PNG'),
+      width: 300,
+      );
+  }
+
+   Widget _showEmail() {
+     return Container(
+       padding: EdgeInsets.all(20),
+       child: TextField(
+         autofocus: true,
+         keyboardType: TextInputType.emailAddress,
+         decoration: InputDecoration(
+           hintText: 'Ingresa tu email...',
+           labelText: 'Email',
+           suffixIcon: Icon(Icons.email),
+           border: OutlineInputBorder(
+             borderRadius: BorderRadius.circular(10)
+           )
+         ),
+         onChanged: (value) {
+           _email = value;
+           print(_email);
+         },
+       ),
+     );
+   }
+
+ 
 }
